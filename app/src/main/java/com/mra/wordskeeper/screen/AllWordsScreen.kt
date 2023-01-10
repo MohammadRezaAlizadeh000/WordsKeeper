7f2036319716c417b702519c25e43c034595444c
mra <mohammad.reza.alizadeh3030@gmail.com>
2023-01-09 23:19:15 -0800

    Initial Commit

---
+++ app/src/main/java/com/mra/wordskeeper/screen/AllWordsScreen.kt
@@ -0,0 +1,12 @@
+package com.mra.wordskeeper.screen
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+
+@Composable
+fun AllWordsScreen(viewModel: ViewModel) {
+
+
+
+}
---
+++ app/src/main/java/com/mra/wordskeeper/screen/allWordsScreen/AllWordsViewModel.kt
@@ -0,0 +1,7 @@
+package com.mra.wordskeeper.screen.allWordsScreen
+
+import androidx.lifecycle.ViewModel
+
+class AllWordsViewModel: ViewModel() {
+
+}
